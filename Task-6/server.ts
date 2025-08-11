b86a1c81a8ccfc762a7dbd4654dc4a16f8159ac2
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2025-08-11 23:16:20 +0300

    Task-6 Solution

---
+++ Task-6/server.ts
@@ -0,0 +1,21 @@
+import http from "node:http";
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  if (req.method === "GET" && req.url === "/") {
+    res.statusCode = 200;
+    res.end(JSON.stringify({ message: "Welcome to the server" }));
+  } else if (req.method === "GET" && req.url === "/about") {
+    res.statusCode = 200;
+    res.end(JSON.stringify({ message: "This is the about route" }));
+  } else {
+    res.statusCode = 404;
+    res.end(JSON.stringify({ error: "Route not found" }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
